54b110dbf3ff54acdfda928f34482cd81c64b7e5
mrmag518 <magnusaub@yahoo.no>
2013-12-14 17:44:25 +0100

    Ups

--- src/com/mrmag518/SpawnerGUI/GUIClickEvent.java
+++ src/com/mrmag518/SpawnerGUI/GUIClickEvent.java
@@ -47,4 +47,8 @@ public class GUIClickEvent extends Event {
     public HandlerList getHandlers() {
         return handlers;
     }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 }
